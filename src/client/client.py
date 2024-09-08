84aa87eb140780030571d43c267649608bd23fa8
piotr <261696@student.pwr.edu.pl>
2024-09-08 22:36:57 +0200

    xml prototypes

--- src/client/client.py
+++ src/client/client.py
@@ -1,27 +1,54 @@
 import socket
 import threading
+import os
 
-def receive_messages(client_socket):
+def receive_files(client_socket):
     while True:
         try:
-            message = client_socket.recv(1024)
-            if not message:
+            file_info = client_socket.recv(1024).decode()
+            if not file_info:
                 break
-            print(f"Received: {message.decode()}")
+            
+            file_name, file_size = file_info.split(':')
+            file_size = int(file_size)
+            
+            print(f"Receiving file: {file_name} of size: {file_size} bytes")
+            
+            with open(f"received_{file_name}", 'wb') as file:
+                received_size = 0
+                while received_size < file_size:
+                    data = client_socket.recv(1024)
+                    if not data:
+                        break
+                    file.write(data)
+                    print(data)
+                    received_size += len(data)
+            
+            print(f"Received file: {file_name}")
         except:
             break
+
+def send_file(client_socket, file_path):
+    file_name = os.path.basename(file_path)
+    file_size = os.path.getsize(file_path)
+    
+    client_socket.send(f"{file_name}:{file_size}".encode())
+    
+    with open(file_path, 'rb') as file:
+        while (data := file.read(1024)):
+            client_socket.send(data)
 
 def start_client(server_ip, server_port):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((server_ip, server_port))
     print(f"Connected to server at {server_ip}:{server_port}")
 
-    threading.Thread(target=receive_messages, args=(client_socket,)).start()
+    threading.Thread(target=receive_files, args=(client_socket,)).start()
 
     try:
         while True:
-            message = input("Enter message: ")
-            client_socket.send(message.encode())
+            file_path = input("Enter path to XML file to send: ")
+            send_file(client_socket, file_path)
     except KeyboardInterrupt:
         print("Disconnecting from server.")
     finally:
@@ -29,5 +56,5 @@ def start_client(server_ip, server_port):
 
 if __name__ == "__main__":
     server_ip = '0.0.0.0'  # Replace with the actual server IP
-    server_port = 12345
+    server_port = 12346
     start_client(server_ip, server_port)
--- src/server/server.py
+++ src/server/server.py
@@ -1,6 +1,7 @@
 import socket
+import os
 
-def start_server(host='0.0.0.0', port=12345):
+def start_server(host='0.0.0.0', port=12346):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((host, port))
     server_socket.listen(5)
@@ -23,19 +24,37 @@ def start_server(host='0.0.0.0', port=12345):
 def handle_client(client_socket, clients):
     try:
         while True:
-            message = client_socket.recv(1024)
-            if not message:
+            file_info = client_socket.recv(1024).decode()
+            if not file_info:
                 break
-            print(f"Received: {message.decode()}")
-            broadcast(message, clients, client_socket)
+            
+            file_name, file_size = file_info.split(':')
+            file_size = int(file_size)
+            
+            print(f"Receiving file: {file_name} of size: {file_size} bytes")
+            
+            with open(file_name, 'wb') as file:
+                received_size = 0
+                while received_size < file_size:
+                    data = client_socket.recv(1024)
+                    if not data:
+                        break
+                    file.write(data)
+                    received_size += len(data)
+            
+            print(f"Received file: {file_name}")
+            broadcast(file_name, file_size, clients, client_socket)
     finally:
         client_socket.close()
 
-def broadcast(message, clients, source_socket):
+def broadcast(file_name, file_size, clients, source_socket):
     for client in clients:
         if client != source_socket:
             try:
-                client.send(message)
+                client.send(f"{file_name}:{file_size}".encode())
+                with open(file_name, 'rb') as file:
+                    while (data := file.read(1024)):
+                        client.send(data)
             except:
                 clients.remove(client)
 
